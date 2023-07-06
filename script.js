c6971c65d43d5fee32031a1f88d1332ec3063db6
AdminK <volvenkova.k@gmail.com>
2023-07-06 22:54:17 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// ДЗ 44. Як отримати дату+час з мілісекунд іншим стособом ніж в доданому коду?
+// let startDate = new Date('2023-06-23');
+// let endDate = new Date('2023-06-30');
+// let timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+// console.log(timeDiff);
+// let daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24)); // як зробити це ж використовуючи Date
+// console.log(daysDiff);
+
+
+let startDate = new Date('2023-06-23');
+let endDate = new Date('2023-06-30');
+
+let daysDiff = 0;
+
+while (startDate < endDate) {
+    startDate.setDate(startDate.getDate() + 1);
+    daysDiff++;
+};
+
+console.log(daysDiff);
